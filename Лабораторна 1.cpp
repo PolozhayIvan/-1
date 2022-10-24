d2ea31a69c9efaf5f5a1286e06a3a3f0ed87a622
PolozhayIvan <polozhay.04@gmail.com>
2022-10-24 23:52:08 +0300

    Я сделаль

---
+++ Лабораторна 1.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <Windows.h> 
+using namespace std;
+int main()
+{
+    cout << "Task HELLO" << endl;
+    cout << endl;
+    cout << "HELLO!" << endl;
+    cout << "--------------" << endl;
+
+
+    cout << "Task SQUARE" << endl;
+    cout << char(201) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(187) << endl;
+    cout << char(186) << ' ' << ' ' << ' ' << ' ' << ' ' << ' ' << ' ' << char(186) << endl;
+    cout << char(186) << ' ' << ' ' << ' ' << ' ' << ' ' << ' ' << ' ' << char(186) << endl;
+    cout << char(186) << ' ' << ' ' << ' ' << ' ' << ' ' << ' ' << ' ' << char(186) << endl;
+    cout << char(200) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(188) << endl;
+
+
+
+
+    cout << "----------------" << endl;
+    cout << "Task ANKETA" << endl;
+    cout << " " << endl;
+    cout << ' ' << ' ' << ' ' << ' ' << ' ' << ' ' << ' ' << "Surname" << ' ' << ":" << ' ' << "Polozhay" << endl;
+    cout << ' ' << ' ' << ' ' << ' ' << ' ' << ' ' << ' ' << ' ' << ' ' << ' ' << "Name" << ' ' << ":" << ' ' << "Ivan" << endl;
+    cout << ' ' << ' ' << ' ' << ' ' << "Patronymic" << ' ' << ":" << ' ' << "Olexandrovich" << endl;
+    cout << ' ' << ' ' << ' ' << ' ' << ' ' << ' ' << ' ' << ' ' << "Gender" << ' ' << ":" << ' ' << "Male" << endl;
+    cout << ' ' << "Date of birth" << ' ' << ":" << ' ' << "21.03.2004" << endl;
+    cout << ' ' << ' ' << ' ' << ' ' << ' ' << ' ' << ' ' << "Address" << ' ' << ":" << ' ' << "c.Novomoskovsk,House 5" << endl;
+    cout << ' ' << ' ' << "Phone number" << ' ' << ":" << ' ' << "+380 67 8080245" << endl;
+    cout << "Place of study" << ' ' << ":" << ' ' << "MMF,DNU nam.Oles Gonchar" << endl;
+    cout << ' ' << ' ' << ' ' << ' ' << ' ' << ' ' << ' ' << ' ' << ' ' << "Group" << ' ' << ":" << ' ' << "MS-21-1" << endl;
+    cout << ' ' << ' ' << ' ' << ' ' << ' ' << ' ' << ' ' << ' ' << ' ' << "Hobby" << ' ' << ":" << ' ' << "Swimming" << endl;
+    cout << "----------------" << endl;
+
+}
